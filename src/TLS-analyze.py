88aa4130f5e89a744fa8ec3ab2ca2dd4abc019cd
kagakusixya <kaeruuta1010@gmail.com>
2020-01-29 00:11:30 +0900

    整形

--- src/TLS-analyze.py
+++ src/TLS-analyze.py
@@ -3,9 +3,7 @@ class TLS_Analyze:
         Define()
         TLS_Record_Layer()
 
-
-
     def Define(self):
         self.content_type = {"change_cipher_spec": b'\x14', "alert": b'\x15',
-                            "handshake": b'\x16', "application_data": b'\x17'}
-        self.protocol_version =  {"TLS1.0":b'\x03\x01',"TLS1.2":b'\x03\x03'}
+                             "handshake": b'\x16', "application_data": b'\x17'}
+        self.protocol_version = {"TLS1.0": b'\x03\x01', "TLS1.2": b'\x03\x03'}
