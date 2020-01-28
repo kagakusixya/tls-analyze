7036d40d36abd87fe02551d5eeb3a727b57a3d7e
kagakusixya <kaeruuta1010@gmail.com>
2020-01-29 00:10:30 +0900

    Defineにversionを追加

--- src/TLS-analyze.py
+++ src/TLS-analyze.py
@@ -1,6 +1,11 @@
 class TLS_Analyze:
     def __init__(self):
         Define()
+        TLS_Record_Layer()
+
+
 
     def Define(self):
-        self.contenttype = {"change_cipher_spec":'\x14',"alert":'\x15',"handshake":'\x16',"application_data":'\x17'}
+        self.content_type = {"change_cipher_spec": b'\x14', "alert": b'\x15',
+                            "handshake": b'\x16', "application_data": b'\x17'}
+        self.protocol_version =  {"TLS1.0":b'\x03\x01',"TLS1.2":b'\x03\x03'}
