bc1c4ea794d1173482c85ad1f8eba9580074e964
kagakusixya <kaeruuta1010@gmail.com>
2020-02-02 18:50:08 +0900

    Extensionの修正

--- src/TLS-analyze.py
+++ src/TLS-analyze.py
@@ -38,14 +38,29 @@ class TLS_Analyze:
         self.ciper_suites = b'\xc0\x2c\xc0\x30\xc0\x2b\xc0\x2f\xcc\xa9\xcc\xa8\x00\x9f\x00\x9e\xcc\xaa\xc0\xaf\xc0\xad\xc0\xae\xc0\xac\xc0\x24\xc0\x28\xc0\x23\xc0\x27\xc0\x0a\xc0\x14\xc0\x09\xc0\x13\xc0\xa3\xc0\x9f\xc0\xa2\xc0\x9e\x00\x6b\x00\x67\x00\x39\x00\x33\x00\x9d\x00\x9c\xc0\xa1\xc0\x9d\xc0\xa0\xc0\x9c\x00\x3d\x00\x3c\x00\x35\x00\x2f\x00\xff'
         self.compression_methods_length = b'\x01'
         self.compression_methods = b'\x00'
+        self.extension_length = b'\x00\x00'
 
     def Handshake_Body_byte(self):
         byte_data = self.handshak_version + self.random + self.session_id_length + self.session_id + \
             self.ciper_suites_length + self.ciper_suites + \
-            self.compression_methods_length + self.compression_methods
+            self.compression_methods_length + self.compression_methods + self.extension_length
         return byte_data
 
+    def Extension(self):
+        self.ec_point_formats = b'\x00\x0b\x00\x04\x03\x00\x01\x02'
+        self.supported_groups = b'\x00\x0a\x00\x04\x00\x02\x00\x17'
+        self.sessionticket_tls = b'\x00\x23\x00\x00'
+        self.encrypt_then_mac = b'\x00\x16\x00\x00'
+        self.extended_master_secret = b'\x00\x17\x00\x00'
+        self.signature_algorithms = b'\x00\x0d\x00\x20\x00\x1e\x06\x01\x06\x02\x06\x03\x05\x01\x05\x02\x05\x03\x04\x01\x04\x02\x04\x03\x03\x01\x03\x02\x03\x03\x02\x01\x02\x02\x02\x03'
+
+    def Extension_byte(self):
+        self.ec_point_formats + self.supported_groups + self.sessionticket_tls + \
+            self.encrypt_then_mac + self.extended_master_secret + self.signature_algorithms
+
     def ssl_len(self):
+        self.extension_length = len(
+            self.Extension_byte()).to_bytes(2, 'big')  # length is 2
         self.compression_methods_length = len(
             self.compression_methods).to_bytes(1, 'big')  # length is 1
 
@@ -84,6 +99,7 @@ def main():
         tls.TLS_Record_Layer()
         tls.Handshake_Header()
         tls.Handshake_Body()
+        tls.Extension_byte()
         tls.ssl_len()
         tls_byte = tls.TLS_Record_Layer_byte() + tls.Handshake_Header_byte() + \
             tls.Handshake_Body_byte()
