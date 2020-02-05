30cc6e5e090e1de6601c88037d4d41192c1f66a8
kagakusixya <kaeruuta1010@gmail.com>
2020-02-05 22:09:47 +0900

    不必要なdefを消去

--- src/TLS-analyze.py
+++ src/TLS-analyze.py
@@ -96,15 +96,11 @@ class TLS_Analyze:
             sum = x.to_bytes(1, 'big') + bytes(sum)
         return sum
 
-    def packet_analyze(self, tls_analyze, str):
-        return tls_analyze
-
     def separate_str(str, len):
         separate_data = b''
         for i in range(len):
             separate_data = str[i] + separate_data
-    return
-
+        return separate_data
 
 def main():
     port = 443
