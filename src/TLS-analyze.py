8b22866287fcf65d2f51e539d4630de9bee54e24
kagakusixya <kaeruuta1010@gmail.com>
2020-02-16 21:40:57 +0900

    分けた

--- src/TLS-analyze.py
+++ src/TLS-analyze.py
@@ -8,77 +8,6 @@ from TLS_Struct import *
 
 class TLS_Analyze:
 
-    def TLS_Record_Layer(self):
-        self.content_type = Define().define_content_type["handshake"]
-        self.version = Define().define_protocol_version["TLS1.0"]
-        self.length = b'\x00\x00'
-
-    def TLS_Record_Layer_byte(self):
-        byte_data = self.content_type + self.version + self.length
-        return byte_data
-
-    def Handshake_Header(self):
-        self.handshake_type = Define().define_handshake_type["client_hello"]
-        self.handshak_length = b'\x00\x00\x00'
-
-    def Handshake_Header_byte(self):
-        byte_data = self.handshake_type + self.handshak_length
-        return byte_data
-
-    def Client_Hello(self):
-        self.handshak_version = Define().define_protocol_version["TLS1.2"]
-        self.random = make_random()
-        self.session_id_length = b'\x00'
-        self.session_id = b''
-        self.ciper_suites_length = b''
-        self.ciper_suites = b'\xc0\x2c\xc0\x30\xc0\x2b\xc0\x2f\xcc\xa9\xcc\xa8\x00\x9f\x00\x9e\xcc\xaa\xc0\xaf\xc0\xad\xc0\xae\xc0\xac\xc0\x24\xc0\x28\xc0\x23\xc0\x27\xc0\x0a\xc0\x14\xc0\x09\xc0\x13\xc0\xa3\xc0\x9f\xc0\xa2\xc0\x9e\x00\x6b\x00\x67\x00\x39\x00\x33\x00\x9d\x00\x9c\xc0\xa1\xc0\x9d\xc0\xa0\xc0\x9c\x00\x3d\x00\x3c\x00\x35\x00\x2f\x00\xff'
-        self.compression_methods_length = b'\x01'
-        self.compression_methods = b'\x00'
-        self.extension_length = b'\x00\x00'
-        self.extensions = b''
-
-    def Client_Hello_byte(self):
-        byte_data = self.handshak_version + self.random + self.session_id_length + self.session_id + \
-            self.ciper_suites_length + self.ciper_suites + \
-            self.compression_methods_length + self.compression_methods + \
-            self.extension_length + self.extensions
-        return byte_data
-
-    def Extension(self):
-        self.ec_point_formats = b'\x00\x0b\x00\x04\x03\x00\x01\x02'
-        self.supported_groups = b'\x00\x0a\x00\x04\x00\x02\x00\x17'
-        self.sessionticket_tls = b'\x00\x23\x00\x00'
-        self.encrypt_then_mac = b'\x00\x16\x00\x00'
-        self.extended_master_secret = b'\x00\x17\x00\x00'
-        self.signature_algorithms = b'\x00\x0d\x00\x20\x00\x1e\x06\x01\x06\x02\x06\x03\x05\x01\x05\x02\x05\x03\x04\x01\x04\x02\x04\x03\x03\x01\x03\x02\x03\x03\x02\x01\x02\x02\x02\x03'
-
-    def Extension_byte(self):
-        byte_data = self.ec_point_formats + self.supported_groups + self.sessionticket_tls + \
-            self.encrypt_then_mac + self.extended_master_secret + self.signature_algorithms
-        return byte_data
-
-    def ssl_len(self):
-
-        if hasattr(self, 'extension_length'):
-            self.extension_length = len(
-                self.Extension_byte()).to_bytes(Define().define_size["extension_length"], 'big')  # length is 2
-
-        if hasattr(self, 'compression_methods_length'):
-            self.compression_methods_length = len(
-                self.compression_methods).to_bytes(Define().define_size["compression_methods_length"], 'big')  # length is 1
-
-        if hasattr(self, 'session_id_length'):
-            self.session_id_length = len(
-                self.session_id).to_bytes(Define().define_size["session_id_length"], 'big')  # length is 2
-
-        if hasattr(self, 'ciper_suites_length'):
-            self.ciper_suites_length = len(
-                self.ciper_suites).to_bytes(Define().define_size["ciper_suites_length"], 'big')  # length is 2
-
-        if hasattr(self, 'handshak_length'):
-            self.handshak_length = len(
-                self.Client_Hello_byte()).to_bytes(Define().define_size["handshak_length"], 'big')
-
     def Separate_Str(self, str, point_length, len):
         separate_data = b''
         for i in range(len):
@@ -89,38 +18,35 @@ class TLS_Analyze:
 
     def Analyze_Packet(self, str):
 
+        tls_record_layer = TLS_Record_Layer()
+        handshake_header = Handshake_Header()
         point_length = 0
 
-        point_length, self.content_type = self.Separate_Str(
+        point_length, tls_record_layer.content_type = self.Separate_Str(
             str, point_length, Define().define_size["content_type"])
         content_type_str = analyze_dict(
-            self.content_type, Define().define_content_type)
+            tls_record_layer.content_type, Define().define_content_type)
         print("content_type :  %s" % content_type_str)
 
-        point_length, self.version = self.Separate_Str(
+        point_length, tls_record_layer.version = self.Separate_Str(
             str, point_length, Define().define_size["version"])
         version_str = analyze_dict(
-            self.version, Define().define_protocol_version)
+            tls_record_layer.version, Define().define_protocol_version)
         print("version : %s" % version_str)
 
-        point_length, self.length = self.Separate_Str(
+        point_length, tls_record_layer.length = self.Separate_Str(
             str, point_length, Define().define_size["length"])
-        print("length : %d" % int.from_bytes(self.length, 'big'))
+        print("length : %d" % int.from_bytes(tls_record_layer.length, 'big'))
 
         # Handshake_Header
-        point_length, self.handshake_type = self.Separate_Str(
+        point_length, handshake_header.handshake_type = self.Separate_Str(
             str, point_length, Define().define_size["handshake_type"])
         handshake_type_str = analyze_dict(
-            self.handshake_type, Define().define_handshake_type)
+            handshake_header.handshake_type, Define().define_handshake_type)
         print("handshake_type : %s" % handshake_type_str)
 
 
-def make_random():
-    sum = b""
-    for i in range(32):
-        x = random.randrange(256)
-        sum = x.to_bytes(1, 'big') + bytes(sum)
-    return sum
+
 
 
 def analyze_dict(data, dict):
@@ -142,27 +68,22 @@ def main():
             print(err)
             return
         print("tcp connected")
-        tls = TLS_Analyze()
-        tls.Client_Hello()
-        tls.Extension()
-        tls.extensions = tls.Extension_byte()
         tls_record_layer = TLS_Record_Layer()
-        tls.ssl_len()
-
+        client_hello = Client_Hello()
+        client_hello.extensions = client_hello.Extension_byte()
+        client_hello.Client_Hello_len()
         handshake_header = Handshake_Header()
-        handshake_header.Handshake_Header_len(tls.Client_Hello_byte())
-        tls_record_layer.TLS_Record_Layer_len(handshake_header.Handshake_Header_byte() + tls.Client_Hello_byte())
+        handshake_header.Handshake_Header_len(client_hello.Client_Hello_byte())
+        tls_record_layer.TLS_Record_Layer_len(handshake_header.Handshake_Header_byte() + client_hello.Client_Hello_byte())
 
         tls_byte = tls_record_layer.TLS_Record_Layer_byte() + handshake_header.Handshake_Header_byte() + \
-            tls.Client_Hello_byte()
+            client_hello.Client_Hello_byte()
 
         sock.send(tls_byte)
 
         recv_data = sock.recv(2048)
         print("-----recive data-----")
         tls_recv = TLS_Analyze()
-        tls_recv.TLS_Record_Layer()
-        tls_recv.Handshake_Header()
         tls_recv.Analyze_Packet(recv_data)
 
         sleep(3)
--- src/TLS_Struct.py
+++ src/TLS_Struct.py
@@ -1,3 +1,5 @@
+import random
+
 from Define import *
 
 
@@ -14,7 +16,8 @@ class TLS_Record_Layer:
     def TLS_Record_Layer_len(self, str):
         self.length = len(str).to_bytes(Define().define_size["length"], 'big')
 
-class  Handshake_Header:
+
+class Handshake_Header:
     def __init__(self):
         self.handshake_type = Define().define_handshake_type["client_hello"]
         self.handshak_length = b'\x00\x00\x00'
@@ -25,4 +28,64 @@ class  Handshake_Header:
 
     def Handshake_Header_len(self, str):
         if hasattr(self, 'handshak_length'):
-            self.handshak_length = len(str).to_bytes(Define().define_size["handshak_length"], 'big')
+            self.handshak_length = len(str).to_bytes(
+                Define().define_size["handshak_length"], 'big')
+
+
+class Client_Hello:
+    def __init__(self):
+        self.handshak_version = Define().define_protocol_version["TLS1.2"]
+        self.random = make_random()
+        self.session_id_length = b'\x00'
+        self.session_id = b''
+        self.ciper_suites_length = b''
+        self.ciper_suites = b'\xc0\x2c\xc0\x30\xc0\x2b\xc0\x2f\xcc\xa9\xcc\xa8\x00\x9f\x00\x9e\xcc\xaa\xc0\xaf\xc0\xad\xc0\xae\xc0\xac\xc0\x24\xc0\x28\xc0\x23\xc0\x27\xc0\x0a\xc0\x14\xc0\x09\xc0\x13\xc0\xa3\xc0\x9f\xc0\xa2\xc0\x9e\x00\x6b\x00\x67\x00\x39\x00\x33\x00\x9d\x00\x9c\xc0\xa1\xc0\x9d\xc0\xa0\xc0\x9c\x00\x3d\x00\x3c\x00\x35\x00\x2f\x00\xff'
+        self.compression_methods_length = b'\x01'
+        self.compression_methods = b'\x00'
+        self.extension_length = b'\x00\x00'
+        self.extensions = b''
+
+        self.Extension()
+
+    def Client_Hello_byte(self):
+        byte_data = self.handshak_version + self.random + self.session_id_length + self.session_id + \
+            self.ciper_suites_length + self.ciper_suites + \
+            self.compression_methods_length + self.compression_methods + \
+            self.extension_length + self.extensions
+        return byte_data
+
+    def Client_Hello_len(self):
+        if hasattr(self, 'extension_length'):
+            self.extension_length = len(
+                self.Extension_byte()).to_bytes(Define().define_size["extension_length"], 'big')  # length is 2
+
+        if hasattr(self, 'compression_methods_length'):
+            self.compression_methods_length = len(
+                self.compression_methods).to_bytes(Define().define_size["compression_methods_length"], 'big')  # length is 1
+
+        if hasattr(self, 'session_id_length'):
+            self.session_id_length = len(
+                self.session_id).to_bytes(Define().define_size["session_id_length"], 'big')  # length is 2
+
+        if hasattr(self, 'ciper_suites_length'):
+            self.ciper_suites_length = len(
+                self.ciper_suites).to_bytes(Define().define_size["ciper_suites_length"], 'big')  # length is 2
+
+    def Extension(self):
+        self.ec_point_formats = b'\x00\x0b\x00\x04\x03\x00\x01\x02'
+        self.supported_groups = b'\x00\x0a\x00\x04\x00\x02\x00\x17'
+        self.sessionticket_tls = b'\x00\x23\x00\x00'
+        self.encrypt_then_mac = b'\x00\x16\x00\x00'
+        self.extended_master_secret = b'\x00\x17\x00\x00'
+        self.signature_algorithms = b'\x00\x0d\x00\x20\x00\x1e\x06\x01\x06\x02\x06\x03\x05\x01\x05\x02\x05\x03\x04\x01\x04\x02\x04\x03\x03\x01\x03\x02\x03\x03\x02\x01\x02\x02\x02\x03'
+
+    def Extension_byte(self):
+        byte_data = self.ec_point_formats + self.supported_groups + self.sessionticket_tls + \
+            self.encrypt_then_mac + self.extended_master_secret + self.signature_algorithms
+        return byte_data
+def make_random():
+    sum = b""
+    for i in range(32):
+        x = random.randrange(256)
+        sum = x.to_bytes(1, 'big') + bytes(sum)
+    return sum
