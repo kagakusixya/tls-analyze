d35a49dda21abe5e28427a918a6bdd05c923443d
kagakusixya <kaeruuta1010@gmail.com>
2020-03-13 03:09:30 +0900

    実行用のファイルをソースに合わせて更新

--- src/main.py
+++ src/main.py
@@ -17,38 +17,48 @@ def main():
         except ConnectionRefusedError as err:
             print(err)
             return
-        tls_record_layer = TLS_Record_Layer()
-        client_hello = Client_Hello()
-        client_hello.cipher_suites = Define(
-        ).define_cipher_suite["TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"]
-        client_hello.extensions = client_hello.Extension_byte()
-        client_hello.Client_Hello_len()
-        handshake_header = Handshake_Header()
-        handshake_header.handshake_type = Define(
-        ).define_handshake_type["client_hello"]
-        handshake_header.Handshake_Header_len(client_hello.Client_Hello_byte())
-        tls_record_layer.TLS_Record_Layer_len(
-            handshake_header.Handshake_Header_byte() + client_hello.Client_Hello_byte())
 
-        tls_byte = tls_record_layer.TLS_Record_Layer_byte() + handshake_header.Handshake_Header_byte() + \
-            client_hello.Client_Hello_byte()
+        tls_basic = TLS_Basic()
+        #client_hello
+        tls_basic.payload = Client_Hello()
+        tls_basic.payload.cipher_suites = Define(
+        ).define_cipher_suite["TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"]
+        tls_basic.payload.extensions = tls_basic.payload.Extension_byte()
+
+        #handshake_header
+        tls_basic.handshake_header = Handshake_Header()
+        tls_basic.handshake_header.handshake_type = Define(
+        ).define_handshake_type["client_hello"]
+
+        tls_basic.setlen()
+
+        tls_byte = tls_basic.tls_record_layer.TLS_Record_Layer_byte() + tls_basic.handshake_header.Handshake_Header_byte() + \
+            tls_basic.payload.byte()
 
         sock.send(tls_byte)
 
-        tls_recv = TLS_Analyze()
-        point_length = 0
-        tls_basics = {}
-        recv_data = b""
-        while tls_recv.done != 1:  # 0 is completed
-            if tls_recv.done == 2:
-                recv_data = recv_data + sock.recv(6000)
-                tls_recv.done = 0
-            point_length, tls_basic = tls_recv.Analyze_Packet(
-                recv_data, point_length)
-            tls_basics[analyze_dict(tls_basic.handshake_header.handshake_type, Define(
-            ).define_handshake_type)] = tls_basic
+        tls_analyze = TLS_Analyze()
 
-        TLS_Debug().Show(tls_basics["server_key_exchange"])
+        sock.settimeout(0.5)
+        recv_segment = b''
+        recv_all = b''
+        while 1 :
+            try:
+                recv_segment = sock.recv(1500)
+            except socket.timeout :
+                break
+            recv_all = recv_all  + recv_segment
+            print(len(recv_segment))
+
+        while tls_analyze.done == 0:  # 0 is completed
+
+            tls_analyze.point_length, tls_basic = tls_analyze.Analyze_Packet(
+                recv_all, tls_analyze.point_length)
+
+            TLS_Debug().Show(tls_basic)
+
+            tls_analyze.tls_basics[analyze_dict(tls_basic.handshake_header.handshake_type, Define(
+            ).define_handshake_type)] = tls_basic
 
 if __name__ == '__main__':
     main()
